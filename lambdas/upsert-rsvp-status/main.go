3547346eb039ac4b1785e26396a83612820319e0
Ben Morris <Ben.Morris@idwalmarine.com>
2024-06-09 20:49:11 +0100

    Use env variable for db ssm parameter name

--- lambdas/upsert-rsvp-status/main.go
+++ lambdas/upsert-rsvp-status/main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	"context"
+	"os"
+	"fmt"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/rs/zerolog/log"
 	"github.com/aws/aws-sdk-go-v2/service/ssm"
 )
+
+const GUEST_INFO_PARAMETER_NAME="GUEST_INFO_PARAMETER_NAME"
 
 func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	config, err := config.LoadDefaultConfig(ctx)
@@ -17,11 +21,17 @@ func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events
 		return response(500, "Error when loading AWS SDK")
 	}
 
+	db_paramter_name, exists := os.LookupEnv(GUEST_INFO_PARAMETER_NAME)
+
+	if !exists {
+		return response(500, fmt.Sprintf("Lambda is missing %s environment variable", GUEST_INFO_PARAMETER_NAME))
+	}
+
 	client := ssm.NewFromConfig(config);
 
 	// Fetch DB name at runtime to get latest value. AWS options exist to reduce latency for fetching configuration values but this allows free tier usage
 	// and the overhead is not a problem for this app currently.
-	dbName, err := get_ssm_parameter("/database/guest_info/table_name", client, ctx)
+	dbName, err := get_ssm_parameter(db_paramter_name, true, client, ctx)
 
 	if err != nil {
 		log.Error().Msg(err.Error());
--- lambdas/upsert-rsvp-status/ssm_handler.go
+++ lambdas/upsert-rsvp-status/ssm_handler.go
@@ -6,9 +6,7 @@ import (
 )
 
 
-func get_ssm_parameter(param_name string, ssm_client *ssm.Client, ctx context.Context) (string, error) {
-	with_decryption := true
-
+func get_ssm_parameter(param_name string, with_decryption bool, ssm_client *ssm.Client, ctx context.Context) (string, error) {
 	get_param_command := ssm.GetParameterInput{
 		Name: &param_name,
 		WithDecryption: &with_decryption,
