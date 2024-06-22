90af5ad3cdcda743642952de5a14c28a0ab0acc0
Ben Morris <Ben.Morris@idwalmarine.com>
2024-06-22 13:19:45 +0100

    Use mockgen for ssm test as well

---
+++ lambdas/upsert-rsvp-status/ssm_test.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/ssm"
+	"github.com/aws/aws-sdk-go-v2/service/ssm/types"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"upsert-rsvp-status/mocks"
+)
+
+func TestGetSSMParameter(t *testing.T) {
+	const expectedValue = "myDbName"
+	expectedError := error(nil)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockClient := mocks.NewMockSSMClientInterface(ctrl)
+
+	ctx := context.TODO()
+	paramName := "testParam"
+
+	expectedInput := &ssm.GetParameterInput{
+		Name:           aws.String(paramName),
+		WithDecryption: aws.Bool(true),
+	}
+
+	mockClient.EXPECT().GetParameter(ctx, expectedInput).Return(&ssm.GetParameterOutput{
+		Parameter: &types.Parameter{Value: aws.String(expectedValue)},
+	}, nil)
+
+	value, err := getSsmParameter(paramName, true, mockClient, ctx)
+
+	assert.Equal(t, value, expectedValue)
+	assert.Equal(t, expectedError, err)
+}
